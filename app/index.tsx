b7175d75f485a416b2cf6bd2e98c10619a8e6af7
Marc985 <hei.marc.3@gmail.com>
2025-02-06 16:08:02 +0300

    chore:add google service json file

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,18 @@
 import { Text, View } from "react-native";
+import React, { useEffect } from 'react';
+import { Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 export default function Index() {
+
+   useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <View
       style={{
@@ -13,3 +25,5 @@ export default function Index() {
     </View>
   );
 }
+
+
